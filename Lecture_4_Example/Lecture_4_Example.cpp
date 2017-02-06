7cba6b61827874f43fec11120f5deb995f1072e9
delaneydi <delaneydi@vcu.edu>
2017-02-06 15:31:01 -0800

    cut and paste lecture example

---
+++ Lecture_4_Example/Lecture_4_Example.cpp
@@ -0,0 +1,24 @@
+// Lecture_4_Example.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int i = 10;
+	int *ptr1 = &i;
+
+	cout << i << endl; 
+
+
+	cout << ptr1 << endl;
+	cout << &i << endl;
+	cout << &ptr1 << endl;
+
+	return 0;
+}
+
+
